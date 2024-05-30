4b3f38ad584aa054c24109fc4fa456c0ed14bf28
yjiiny <yjiiny01@gmail.com>
2024-05-30 18:51:39 +0900

    Initial project setup

---
+++ src/main/java/code/four/devdoc/controller/HelloController.java
@@ -0,0 +1,12 @@
+package code.four.devdoc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/api/test")
+    public String hello() {
+        return "테스트입니다.";
+    }
+}
