fbbe2e936ad8ccc4d67c912ec7686c0f76416d01
yjiiny <yjiiny01@gmail.com>
2024-06-02 22:13:43 +0900

    add tailwindcss, LandingPage

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,37 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-import {useEffect, useState} from "react";
-import axios from "axios";
+import LandingPage from './components/LandingPage';
 
 
 function App() {
-  const [hello, setHello] = useState('');
-  useEffect(() => {
-    axios.get('/api/test')
-        .then((res) => {
-          setHello(res.data);
-        })
-  }, []);
+
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          백엔드 데이터 : {hello}
-        </a>
-      </header>
-    </div>
-  );
+      <div style={{backgroundColor: 'rgba(239, 245, 255, 1)', width: '100vw', height: '100vh', display:'flex', justifyContent: 'center', alignItems: 'center'}}>
+          <LandingPage />
+      </div>
+  )
 }
 
 export default App;
--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -2,10 +2,10 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
     app.use(
-        // React 개발 서버가 /api로 시작하는 요청을 http://localhost:8080으로 프록시
-        '/api',
+        // React 개발 서버가 '/'로 시작하는 요청을 http://localhost:8080으로 프록시
+        '/',
         createProxyMiddleware({
-            target: 'http://localhost:8080',	// 서버 URL or localhost:설정한포트번호
+            target: 'http://localhost:8080',	// 서버 URL or localhost:설정한 포트 번호
             changeOrigin: true,
         })
     );
